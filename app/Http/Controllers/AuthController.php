17e23a25d49aef9e4c86b963106d31be77369036
eddymandran <eddy.mandran@gmail.com>
2022-02-18 11:54:50 +0100

    Signup on Frontend

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,17 +2,24 @@
 
 namespace App\Http\Controllers;
 
+
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rules\Password;
 
+/**
+ * Class AuthController
+ *
+ * @author  Zura Sekhniashvili <zurasekhniashvili@gmail.com>
+ * @package App\Http\Controllers
+ */
 class AuthController extends Controller
 {
-    public function register( Request $request)
+    public function register(Request $request)
     {
         $data = $request->validate([
-            'name' => 'required | string',
-            'email' => 'required | email | string | unique:users, email',
+            'name' => 'required|string',
+            'email' => 'required|email|string|unique:users,email',
             'password' => [
                 'required',
                 'confirmed',
@@ -20,19 +27,17 @@ class AuthController extends Controller
             ]
         ]);
 
-
+        /** @var User $user */
         $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password'])
         ]);
-
         $token = $user->createToken('main')->plainTextToken;
 
-        return response ([
-           'user' => $user,
-           'token' => $token
+        return response([
+            'user' => $user,
+            'token' => $token
         ]);
-
     }
 }
